75befe73b3b78a7aa78f0ea84a660371f7159fc4
tyagihimansh <57464753+tyagihimansh@users.noreply.github.com>
2020-05-24 00:39:11 +0530

    Box Mullar Method

---
+++ P5.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 23 18:01:19 2020
+
+@author: Himanshu Tyagi
+
+Box Mullar method 
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+def y(x):
+    return np.exp(-(x**2)/2)
+x = np.arange(-4,4,0.1)
+y = y(x)*300
+x1 = np.random.rand(10000)
+x2 = np.random.rand(10000)
+y1 = np.sqrt(-2*np.log(x1))*np.cos(2*np.pi*x2)
+y2 = np.sqrt(-2*np.log(x1))*np.sin(2*np.pi*x2)
+plt.subplot(221)
+plt.hist(y1, bins=100)
+plt.plot(x, y, label='Gaussian')
+plt.title('y1')
+plt.legend(prop={'size':8})
+plt.subplot(222)
+plt.hist(y2, bins=100)
+plt.plot(x, y, label='Gaussian')
+plt.legend(prop={'size':8})
+plt.title('y2')
+plt.show()
